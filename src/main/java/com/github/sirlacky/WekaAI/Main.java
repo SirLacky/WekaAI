10e23a33cfeaa15ff262b3880b5be83563feb0f6
Lukasz.Koziara <sirlacky@gmail.com>
2020-03-03 08:09:52 +0100

    Added SparseConverter.

--- src/main/java/com/github/sirlacky/WekaAI/Main.java
+++ src/main/java/com/github/sirlacky/WekaAI/Main.java
@@ -4,15 +4,20 @@ import weka.*;
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
-
+import static main.java.com.github.sirlacky.WekaAI.CSV2ArffConverter.CSV2ArffConverter;
 import static main.java.com.github.sirlacky.WekaAI.AttributeFiler.attributeFilter;
 import static main.java.com.github.sirlacky.WekaAI.LoadSaveData.loadSavedData;
+import static main.java.com.github.sirlacky.WekaAI.SparseConverter.sparseConverter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         //loadSavedData();
 
-        attributeFilter(new String[]{"-R", "2"});
+        //CSV2ArffConverter();
+
+        //attributeFilter(new String[]{"-R", "2"});
+
+        //sparseConverter();
     }
 }
---
+++ src/main/java/com/github/sirlacky/WekaAI/SparseConverter.java
@@ -0,0 +1,39 @@
+package main.java.com.github.sirlacky.WekaAI;
+
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.ConverterUtils;
+import weka.filters.Filter;
+import weka.filters.unsupervised.instance.NonSparseToSparse;
+
+import java.io.File;
+
+/**
+ * Sparse converter converts large files filled mostly with zeros to smaller files without zeros:
+ *
+ * Before: File1:   1,   2,   3, 0, 0, 0, 0, 0, 9;
+ * After:  File2: 0 1, 1 2, 2 3,              8 9; (index, value)
+ *
+ */
+public class SparseConverter {
+    public static void sparseConverter(){
+        try{
+            //Load old dataset
+            ConverterUtils.DataSource source = new ConverterUtils.DataSource("C:\\Users\\lukasz.koziara\\Desktop\\Moje\\Projekty IntelliJ\\WekaAI\\src\\main\\resources\\weather.arff");
+            Instances dataset = source.getDataSet();
+
+            NonSparseToSparse sp = new NonSparseToSparse();
+            sp.setInputFormat(dataset);
+            Instances newData = Filter.useFilter(dataset,sp);
+
+            //Saving new dataset
+            ArffSaver saver = new ArffSaver();
+            saver.setInstances(newData);
+            saver.setFile(new File("C:\\Users\\lukasz.koziara\\Desktop\\Moje\\Projekty IntelliJ\\WekaAI\\src\\main\\resources\\weather2.arff"));
+            saver.writeBatch();
+
+        }catch(Exception e){
+            System.out.println("Sparse Converter Exception");
+        }
+    }
+}
